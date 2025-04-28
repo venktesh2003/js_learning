fe2c2473aa3f800684703b3b146ef6d141af5be3
venktesh2003 <90545558+venktesh2003@users.noreply.github.com>
2025-04-28 07:32:24 +0000

    datatype

---
+++ basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountid = 14425
+let accountEmail = "my@gmail.com"
+var accountpassword = "1244646"
+accountcity = "jaipur"
+
+accountEmail = "Jmahajve@jci.com"
+accountpassword = "xtz"
+accountcity = "pune"
+console.table([accountEmail,accountid , accountpassword,accountcity])
+
+/*
+perfere not to use Var becasue of issue in log and functional scope */
---
+++ basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // this treat all jscode as newer verision
+// alert("hello")  we are using node js not browser
+
+//number =>2 to power 53
+//bigint
+//string =>true / false
+//null =>standlalone value
+//boolean => true / false
+//undefined 
+//symbol =>use to find the uniquesness
+
+
+//objects 
+//typeof()=> it will give type
+
+/*
+  this is interview Quetion
+*/
+//null is type of object
+// typeof(undefined) => undefined
