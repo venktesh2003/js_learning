5cd2d2670530f6d66f4dd7ec487d445f7033f652
venktesh2003 <90545558+venktesh2003@users.noreply.github.com>
2025-04-28 07:56:50 +0000

    typeconverion

---
+++ basic/03_typeconv.js
@@ -0,0 +1,16 @@
+let score = "33";
+
+console.log(typeof score);
+let numval = Number(score);
+console.log(typeof numval)
+
+//consversion to number
+
+// "33" =>33
+//"33abc" =>NaN
+// true  =>1 false =>0
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(typeof somenumber)
+console.log(typeof stringnumber)
