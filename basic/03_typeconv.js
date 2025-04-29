095b90f3ae713f389dff6c810da9a2ddfeef679b
venktesh2003 <90545558+venktesh2003@users.noreply.github.com>
2025-04-29 05:30:04 +0000

    this is last change

--- basic/03_typeconv.js
+++ basic/03_typeconv.js
@@ -1,8 +1,8 @@
-let score = "33";
+// let score = "33";
 
-console.log(typeof score);
-let numval = Number(score);
-console.log(typeof numval)
+// console.log(typeof score);
+// let numval = Number(score);
+// console.log(typeof numval)
 
 //consversion to number
 
@@ -10,7 +10,38 @@ console.log(typeof numval)
 //"33abc" =>NaN
 // true  =>1 false =>0
 
-let somenumber = 33
-let stringnumber = String(somenumber)
-console.log(typeof somenumber)
-console.log(typeof stringnumber)
+// let somenumber = 33
+// let stringnumber = String(somenumber)
+// console.log(typeof somenumber)
+// console.log(typeof stringnumber)
+
+// ==============summaer data type=  ===============================
+
+//type of data 1)primitive ->these are call by value (String , number , boolean , null , undefined , Symbol , BigInt)
+            
+               // 2)Non primitive ->refernce by (Arrays , objects , Functions )
+
+               //java script is dynamic type language 
+               const score1 = 100
+               const score2= 12.333
+               const ourside  = null 
+               let userEmail;   // undefined
+
+               // SYMBOLS  -> used to identify uniquely 
+               const id = Symbol('112')
+               const anotherId = Symbol('112')
+               console.log(id === anotherId)
+//------------no primitive--------------------------
+
+// //Arrays 
+const array = ["1" , 2,5];
+// Object
+let object = {
+    name:"venktesh" , 
+    class: 10 , 
+// }
+
+// function
+let myfunction = function(){
+    console.log("hello ");}
+    
