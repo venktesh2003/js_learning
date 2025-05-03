26ef51d3b0d5959df3503d77ab06de44a52dbbfb
venktesh2003 <90545558+venktesh2003@users.noreply.github.com>
2025-05-03 14:27:39 +0000

    date

---
+++ basic/06_date_and_time.js
@@ -0,0 +1,29 @@
+let mydate= new Date();
+// console.log(mydate.toString())
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+// let mycreateddate = new Date(2023 , 0 ,23 )   // year month date
+// console.log(mycreateddate.toDateString())
+
+// let createmy = new Date(2023 , 3 , 4 , 5, 6);
+// console.log(createmy.toLocaleString())
+
+// // time stamps it is used for fastest ans and booking like scenarions
+
+// let timestamp = Date.now();
+// console.log(timestamp)
+
+// // Date to time stamp we can convert using getTime() method 
+// let mydat = new Date(2023 , 4, 5)
+// console.log(mydat.getTime())
+// console.log(Math.floor(Date.now()/1000))  // for converting it into seconds interview problem
+
+mynewdate = new Date();
+console.log(mynewdate)
+// to get any specific information
+console.log(mynewdate.getMonth())
+
+// creating the date with customization using locale
+
