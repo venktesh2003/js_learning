4cb0bd827978e9b0806801de212891d7362214f6
venktesh2003 <90545558+venktesh2003@users.noreply.github.com>
2025-05-16 17:00:58 +0000

    control flow

---
+++ control_flow/basic.js
@@ -0,0 +1,49 @@
+
+
+/*
+
+ === ( strict checking )  , it will check type and  value as well 
+*/
+
+/*// short hand 
+const balance = 1090
+if(balance > 500 )console.log("test") , console.log("test2") // very immature code
+*/
+// NESTING
+
+const balance = 100 
+if(balance < 500)
+{
+    console.log("less than ")
+}
+else if(balance < 750)
+{
+    console.log("less than 750")
+
+    }
+else{
+    console.log("everything looks good")
+
+}
+
+// switch case
+
+const month = 3
+switch(month)
+{
+    case 1:
+        console.log("january")
+        break;
+        
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("April")
+        break;
+
+}
+
