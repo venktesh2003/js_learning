346acb2ea8689589ddaeea76cdaeb4978d546364
venktesh2003 <90545558+venktesh2003@users.noreply.github.com>
2025-05-16 18:42:52 +0000

    for loops

---
+++ control_flow/3_loops.js
@@ -0,0 +1,28 @@
+// for loops
+
+for( index = 1 ; index<10;index++)
+{
+    console.log(index);
+    
+}
+
+// keywords in loops
+
+// 1) break 2)continue
+
+for(i = 1 ; i<=20;i++)
+{
+    
+    console.log( `${i}`);
+    if(i === 5) break;
+    
+}
+
+for(i = 1 ; i<=20;i++)
+{
+    
+    
+    if(i ==5) continue;
+    console.log( `${i}`);
+    
+}
